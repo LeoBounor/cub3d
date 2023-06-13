915fea72a09e8156035a3a93050eb40932d80264
error7404 <error7404@gmail.com>
2023-06-13 15:16:04 +0200

    fix texture not loading

--- src/parsing/textures_parse.c
+++ src/parsing/textures_parse.c
@@ -6,7 +6,7 @@
 /*   By: lbounor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:07:01 by Leo               #+#    #+#             */
-/*   Updated: 2023/06/13 11:54:09 by lbounor          ###   ########.fr       */
+/*   Updated: 2023/06/13 14:51:11 by lbounor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,18 @@ t_texture	init_texture(t_game *game, char *texture_path)
 	new.texture = mlx_xpm_file_to_image(game->mlx, texture_path, \
 	&new.width_img, &new.height_img);
 	if (!new.texture)
-		if (clear_textures(game))
-			return (free(texture_path), ft_err_map("Error loading texture\n", \
-			game->fd_str, game), new);
+	{
+		free(texture_path);
+		clear_textures(game);
+		ft_err_map("Error loading texture\n", game->fd_str, game);
+		new.texture_addr = NULL;
+		new.width_img = 0;
+		new.height_img = 0;
+		new.bits_per_pixel = 0;
+		new.line_length = 0;
+		new.endian = 0;
+		return (new);
+	}
 	new.texture_addr = mlx_get_data_addr(new.texture, \
 	&new.bits_per_pixel, &new.line_length, &new.endian);
 	new.bits_per_pixel /= 8;
@@ -39,7 +48,7 @@ void	load_texture(t_game *game, t_texture *texture, int *i)
 	skip_to_next_word(game->fd_str, i);
 	tmp = *i;
 	skip_to_eol_or_eof(game->fd_str, i);
-	if (game->fd_str[*i] != '\n')
+	if (game->fd_str[*i] == '\n')
 		*i -= 1;
 	while ((game->fd_str[*i] == ' ' || game->fd_str[*i] == '\t') && *i > tmp)
 		*i -= 1;
